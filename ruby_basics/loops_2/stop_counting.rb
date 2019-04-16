dbe32e394fbdd6d28adbe52f44eed5a68ca712e6
Mohammad Ali <aliawan87@gmail.com>
2019-04-16 21:16:47 +0500

    Ruby Basics/loop_2

---
+++ ruby_basics/loops_2/stop_counting.rb
@@ -0,0 +1,14 @@
+# 5.times do |index|
+# puts index
+# break if index == 2
+# end
+
+# 5.times do |index|
+#   puts index
+#   break if index == 4
+# end
+
+5.times do |index|
+  puts index
+  break if index < 7
+end
